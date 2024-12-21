6a23378bd025bdcef5f50143cc35dd7fea765ee7
DylanAkp <59843812+DylanAkp@users.noreply.github.com>
2024-12-22 00:25:05 +0100

    Themes: Make accent color accessible

--- src/style/Themes.tsx
+++ src/style/Themes.tsx
@@ -4,14 +4,15 @@ export const theme = {
       secondary: '#0d1116',
       third: '#2e2f36',
       text: '#dcdde4',
+      accent: '#f5a747'
     },
     light: {
       primary: '#f1f0f7',
       secondary: '#e3e2e9',
       third: '#c3c4d0',
       text: '#1b1a21',
+      accent: '#f5a747'
     },
-    accent: '#f5a747'
 }
 
 export type ThemeType = typeof theme.light;
