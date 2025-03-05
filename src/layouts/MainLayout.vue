e3cdb0b768159d5972a212c9c9ddcce671988e6f
DylanAkp <59843812+DylanAkp@users.noreply.github.com>
2025-03-05 19:22:30 +0100

    SearchBar: Add searchbar widget

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="main-layout">
     <MainSidebar />
-    <router-view />
+    <div class="main-content">
+      <SearchBar />
+      <router-view />
+    </div>
   </div>
 </template>
 
 <script setup>
 import MainSidebar from 'src/widgets/MainSidebar.vue'
+import SearchBar from 'src/widgets/SearchBar.vue'
 </script>
 
 <style lang="scss" scoped>
 .main-layout {
   display: flex;
+  flex-direction: row;
   height: 100vh;
   width: 100vw;
+  .main-content {
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+  }
 }
 </style>
---
+++ src/widgets/SearchBar.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="search-bar">
+    <input class="search-input" type="text" placeholder="Search" />
+    <div class="search-icon">
+      <q-icon size="25px" name="mdi-magnify" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.search-bar {
+  background-color: var(--secondary-bg);
+  width: fit-content;
+  border-radius: 30px;
+  padding: 5px;
+  display: flex;
+  gap: 10px;
+  justify-content: center;
+  align-items: center;
+  margin-right: auto;
+  margin-left: auto;
+
+  .search-input {
+    width: 300px;
+    height: 50px;
+    border-radius: 25px;
+    border: none;
+    padding: 0 20px;
+    background-color: var(--tertiary-bg);
+    color: var(--text-color);
+    font-size: 1rem;
+    outline: none;
+    transition: all 0.3s ease;
+
+    &::placeholder {
+      color: var(--text-color);
+      opacity: 0.7;
+    }
+  }
+
+  .search-icon {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--tertiary-bg);
+    width: 50px;
+    height: 50px;
+    padding: 10px;
+    border-radius: 50%;
+    cursor: pointer;
+    transition: all 0.3s ease;
+  }
+}
+</style>
